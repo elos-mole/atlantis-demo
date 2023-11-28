86ac819c2779b4082fa0182dc24604b0bdbdf6ae
Jaroslav Svoboda (Krtek) <jaroslav.svoboda@elostech.cz>
2023-11-28 19:54:10 +0100

    feat: Create config repo

--- modules/project/main.tf
+++ modules/project/main.tf
@@ -2,3 +2,16 @@ resource "azuredevops_project" "project" {
   name        = var.project_name
   description = var.project_description
 }
+
+resource "azuredevops_git_repository" "example" {
+  project_id = azuredevops_project.project.id
+  name       = "gitops-configuration"
+  initialization {
+    init_type = "Clean"
+  }
+  lifecycle {
+    ignore_changes = [
+      initialization
+    ]
+  }
+}
