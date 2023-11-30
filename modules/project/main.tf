486b1bc8cb3d93f8517e70398ddbf3ca7b8c2aef
Jaroslav Svoboda (Krtek) <jaroslav.svoboda@elostech.cz>
2023-11-30 08:37:24 +0100

    feat: Demo description

--- modules/project/main.tf
+++ modules/project/main.tf
@@ -1,6 +1,6 @@
 resource "azuredevops_project" "project" {
   name        = var.project_name
-  description = var.project_description
+  description = "Demo - ${var.project_description}"
 }
 
 resource "azuredevops_git_repository" "configuration" {
